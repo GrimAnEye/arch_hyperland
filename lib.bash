a5823420df3eb48c46a8012bd779e68e0839eeac
GrimAnEye <alfavitk97@gmail.com>
2024-02-18 03:38:57 +0700

    ask function

---
+++ lib.bash
@@ -0,0 +1,39 @@
+#/bin/bash
+
+askForPassword(){
+    for ((;;)); do
+        echo -n "Write password: ">&2
+        read -s A_pass1
+        echo -ne "\nRepeat password: ">&2
+        read -s A_pass2
+
+        if [ -z $A_pass1 ] && [ -z $A_pass2 ]; then
+            echo -e "\nPassword not be empty!\n">&2
+            continue
+        fi
+        fi [ ! "$A_pass1" == "$A_pass2" ]; then
+            echo -e "\nPasswords is different!\n">&2
+            continue
+        fi
+        echo "">&2
+        break
+    done
+echo $A_pass1
+unset A_pass1 A_pass2
+}
+
+askForName(){
+    for ((;;));do
+        echo -n "Write new $1: ">&1
+        read A_name
+
+        if [ -z $A_name ]; then
+            echo -e "$1 not be empty!\n">&2
+            continue
+        fi
+        echo "">&2
+        break
+    done
+echo $A_name
+unset A_name
+}
